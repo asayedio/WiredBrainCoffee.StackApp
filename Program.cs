3334950216af3988c0d3f31cd8afa0a6373765fd
Ahmed Sayed <ahmedsayed.9191@gmail.com>
2021-08-05 07:43:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace WiredBrainCoffee.StackApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackDoubles();
+            StackStrings();
+            Console.ReadLine();
+        }
+
+        private static void StackDoubles()
+        {
+            var simpleStack = new SimpleStack();
+            double sum = 0.0;
+            simpleStack.Push(1.2);
+            simpleStack.Push(2.3);
+            simpleStack.Push(3.5);
+
+            while (simpleStack.Count > 0)
+            {
+                double item;
+                item = simpleStack.Pop();
+                Console.WriteLine($"Item is: {item}");
+                sum += item;
+            }
+            Console.WriteLine($"The sum is: {sum}");
+        }
+
+        private static void StackStrings()
+        {
+            var simpleStack = new SimpleStack();
+            simpleStack.Push("Ahmed");
+            simpleStack.Push("Sayed");
+            simpleStack.Push("Sobhy");
+
+            while (simpleStack.Count > 0)
+            {
+                Console.WriteLine($"Item is: {simpleStack.Pop();}");
+            }
+        }
+    }
+}
---
+++ SimpleStack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WiredBrainCoffee.StackApp
+{
+    public class SimpleStack
+    {
+        private readonly double[] _items;
+        private int topIndex = 0;
+
+        public SimpleStack() => _items = new double[10];
+        public int Count => topIndex;
+        public void Push(double item) => _items[topIndex++] = item;
+        public double Pop() => _items[--topIndex];
+    }
+}
